85f10f9f5a8dc2a5f465afc73bcb8edd420f03e5
Niko <wrnkt1@gmail.com>
2022-11-26 00:36:11 -0500

    Separate versions of fire spreading logic

--- FramePrinter.java
+++ FramePrinter.java
@@ -14,7 +14,7 @@ class FramePrinter
         {
             for (int y = 1; y < fSource.HEIGHT; y++)
             {
-                spreadFire(fSource, newFrameBuffer, x, y);
+                spreadFireUpRand(fSource, newFrameBuffer, x, y);
             }
         }
 
@@ -22,7 +22,37 @@ class FramePrinter
         return newFrame;
     }
 
-    public static void spreadFire(Frame fSource, int[] newFrameBuffer, int x, int y)
+    public static void spreadFireUpLeftRightRand(Frame fSource, int[] newFrameBuffer, int x, int y)
+    {
+        log("LOG", String.format("length of fSource buffer = %d", fSource.getFrameBuffer().length));
+        log("LOG", String.format("length of newFrameBuffer = %d", newFrameBuffer.length));
+        log("LOG", String.format("x = %d", x));
+        log("LOG", String.format("y = %d", y));
+        int rand = (int) Math.round(Math.random() * 3.0) & 3;
+
+        int srcIndex = xyToBufferIndex(fSource, x, y);
+        int destIndex = srcIndex - fSource.WIDTH - rand + 1;
+
+        // set frameBuffer values' lower limit
+        if (fSource.getFrameBuffer()[destIndex] > 0 &&
+                (srcIndex > 0 && srcIndex < fSource.getFrameBuffer().length) &&
+                (destIndex > 0 && destIndex < newFrameBuffer.length)) 
+            newFrameBuffer[destIndex] = fSource.getFrameBuffer()[srcIndex] + (rand & 1);
+    }
+
+    public static void spreadFireUpRand(Frame fSource, int[] newFrameBuffer, int x, int y)
+    {
+        int srcIndex = xyToBufferIndex(fSource, x, y);
+        int destIndex = srcIndex - fSource.WIDTH;
+
+        int rand = (int) Math.round(Math.random() * 3.0) & 3;
+
+        // set frameBuffer values' lower limit
+        if (fSource.getFrameBuffer()[destIndex] > 0) 
+            newFrameBuffer[destIndex] = fSource.getFrameBuffer()[srcIndex] + (rand & 1);
+    }
+
+    public static void spreadFireUp(Frame fSource, int[] newFrameBuffer, int x, int y)
     {
         int srcIndex = xyToBufferIndex(fSource, x, y);
         int destIndex = srcIndex - fSource.WIDTH;
@@ -104,14 +134,14 @@ class FramePrinter
 
         Frame currentFrame = seedFrame;
 
-        for (int i = 0; i < 20; i++)
+        for (int i = 0; i < 25; i++)
         {
             Frame nextFrame = getNextFireFrame(currentFrame);
             fireTest.add(nextFrame);
             currentFrame = nextFrame;
         }
 
-        printLoop(fireTest, 500);
+        printLoop(fireTest, 200);
 
     }
 }
